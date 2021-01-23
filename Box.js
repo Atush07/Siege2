960d358a98eb130f3b76a8f2f3732b730fa3eab3
Atush <75175817+Atush07@users.noreply.github.com>
2021-01-22 21:09:35 -0800

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,28 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+      stiffness: 0.04,
+      length: 20
+    }  
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.visibility=255;
+    
+    World.add(world, this.body);
+  }
+
+  display(){
+
+    if(this.body.speed<3){
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rectMode(CENTER);
+    fill(995,95,19);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+
+  }
+}
